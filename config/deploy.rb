2a190087ef3290bd734c5bcdd0b54cf6cc1d42ab
sistemascab <estebanhd2010@gmail.com>
2016-09-09 12:15:47 +0000

    Versión de aplicación desplegada

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :repo_url, 'git@github.com:dickson95/confecabproduccion.git'
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
 
+set :use_sudo, true
+
 # Default value for :scm is :git
 # set :scm, :git
 
@@ -21,7 +23,7 @@ set :repo_url, 'git@github.com:dickson95/confecabproduccion.git'
 # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
@@ -35,8 +37,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 set :puma_conf, "#{shared_path}/config/puma.rb"
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
  
 namespace :deploy do
   before 'check:linked_files', 'puma:config'
