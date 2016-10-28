89528b2b240419c26ad1adc079f1020b1850c9a9
Esteban Higuita <estebanhd2010@gmail.com>
2016-10-28 14:28:06 -0500

    Asignar habilidades para los usuarios Pasar los permisos para el ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,21 +1,36 @@
 class Ability
   include CanCan::Ability
-
+  
   def initialize(user)
     user ||= User.new # guest user
+
+    alias_action :total_price, :to => :prices
+    alias_action :view_details, :show, :to => :read
+    alias_action :edit, :update, :to => :billing
+
     if user.has_rol? :admin
       can :manage, :all
+      # Evita que el usuario con el id que se le pase sea eliminado
+      cannot :destroy, User do |u|
+        u.id == 1
+      end
     elsif user.has_rol? :coor_tiempos
-      can :manage, :all
+      can :manage, [Programacion, User, ControlLote, Lote, SubEstado, Cliente, TipoPrenda]
     elsif user.has_rol? :coor_integracion
-      can [:manage], [Lote, Programacion, ControlLote]
-      cannot :total_price, Lote
+      can :manage, [Programacion, ControlLote, Lote]
+      can :create, [Cliente, TipoPrenda]
+      cannot :prices, Lote
     elsif user.has_rol? :aux_insumos
-      can [:read, :create, :update], Lote
-      can [:read], Programacion
+      can [:read, :update], Lote
+      can [:read, :export], Programacion
+      can :read, [ControlLote, Cliente]
     elsif user.has_rol? :gerente
       can :read, :all
-      can :total_price, Lote
+      can [:read, :export], Programacion
+      can :prices, Lote
+      cannot :manage, [Referencia, Talla, Rol]
+    elsif user.has_rol? :aux_facturacion
+      can [:read, :billing], Lote
     end
   end
 end
